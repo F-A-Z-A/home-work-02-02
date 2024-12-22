f06e1e846a333ff4145ae64a2074d9c1c407669d
AndreyUrazaev <f.a.z.a@bk.ru>
2024-12-22 16:04:13 +0500

    HomeWork-04-03 (HW11)

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -4,14 +4,32 @@ import { Slider, SliderProps } from "@mui/material";
 const SuperRange: React.FC<SliderProps> = (props) => {
   return (
     <Slider
-      sx={
-        {
-          // стили для слайдера // пишет студент
-        }
-      }
-      {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+      sx={{
+        width: 150,
+        color: "#3bc34b",
+      }}
+      {...props}
     />
   );
 };
 
 export default SuperRange;
+
+// ---------------------------------------------------------------------
+// import React from "react";
+// import { Slider, SliderProps } from "@mui/material";
+//
+// const SuperRange: React.FC<SliderProps> = (props) => {
+//   return (
+//     <Slider
+//       sx={
+//         {
+//           // стили для слайдера // пишет студент
+//         }
+//       }
+//       {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+//     />
+//   );
+// };
+//
+// export default SuperRange;
