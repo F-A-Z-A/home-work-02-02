390e7942eef0150fcf3ce1c6b5518ca051239612
AndreyUrazaev <f.a.z.a@bk.ru>
2024-12-31 12:16:06 +0500

    HomeWork-04-04 (HW12)

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -19,7 +19,7 @@ function HW11() {
 
   return (
     <div id={"hw11"}>
-      <div className={s2.hwTitle}>Homework #11</div>
+      <div className={s2.hwTitle}>Hometask № 11</div>
 
       <div className={s2.hw}>
         <div className={s.container}>
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,14 +2,33 @@ const initState = {
   themeId: 1,
 };
 
-export const themeReducer = (state = initState, action: any): any => {
-  // fix any
+export const themeReducer = (state: InitStateType = initState, action: ActionsType): InitStateType => {
   switch (action.type) {
-    // дописать
-
+    case "SET_THEME_ID":
+      return { ...state, themeId: action.id };
     default:
       return state;
   }
 };
 
-export const changeThemeId = (id: number): any => ({ type: "SET_THEME_ID", id }); // fix any
+export const changeThemeId = (id: number) => ({ type: "SET_THEME_ID", id }) as const;
+
+type InitStateType = typeof initState;
+type ActionsType = ReturnType<typeof changeThemeId>;
+
+// ---------------------------------------------------------------------
+// const initState = {
+//   themeId: 1,
+// };
+//
+// export const themeReducer = (state = initState, action: any): any => {
+//   // fix any
+//   switch (action.type) {
+//     // дописать
+//
+//     default:
+//       return state;
+//   }
+// };
+//
+// export const changeThemeId = (id: number): any => ({ type: "SET_THEME_ID", id }); // fix any
